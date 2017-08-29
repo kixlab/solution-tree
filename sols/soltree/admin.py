0ace10ddfde21193bf0dab21ffdc433f48d42b61
wellshs <wellshs999@gmail.com>
2017-08-29 13:01:47 +0900

    Refactoring -database change

--- sols/soltree/admin.py
+++ sols/soltree/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import solution, node, sub_how_to
+from .models import problem, solution, node, annotation, node_text, sub_how_to
 
+admin.site.register(problem)
 admin.site.register(solution)
 admin.site.register(node)
 admin.site.register(sub_how_to)
+admin.site.register(annotation)
+admin.site.register(node_text)
 
 
 # Register your models here.
--- sols/soltree/models.py
+++ sols/soltree/models.py
@@ -1,23 +1,36 @@
 from django.db import models
 from django.core.urlresolvers import reverse_lazy
 
+class problem(models.Model):
+    img = models.ImageField(upload_to='problem')
+    text = models.CharField(max_length=1000)
+
 class solution(models.Model):
     img = models.ImageField(upload_to='orig')
-
+    tagged_img = models.ImageField(upload_to='edit', null=True, blank=True)
+    problem_pk = models.ForeignKey(problem, on_delete=models.CASCADE, null=True)
     def get_absolute_url(self):
         url = reverse_lazy('tag', kwargs={'pk': self.pk})
         return url
 
+
 class node(models.Model):
     parentId = models.ForeignKey('self', null=True, blank=True)
     summarization = models.CharField(max_length=100)
-
     def __str__(self):
         return self.summarization
 
+class annotation(models.Model):
+    node_pk = models.ForeignKey(node, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    # is_instructor = models.BooleanField()
+
+class node_text(models.Model):
+    node_pk = models.ForeignKey(node, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+
 class sub_how_to(models.Model):
     orig_sol = models.ForeignKey(solution, on_delete=models.CASCADE)
-    node_id = models.ForeignKey(node)
-    img_slice = models.ImageField(upload_to='slice')
+    text = models.CharField(max_length=100, null=True)
     order = models.IntegerField()
 # Create your models here.
